0fdad1a8f3ffee9ca516a616bca20e17ab3f59db
Thiago Ribeiro <mackandalls@gmail.com>
2024-08-05 12:53:26 -0300

    Initial commit: setup FastAPI application with file upload functionality

---
+++ audio_app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request, Form, UploadFile, File
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import os
+
+app = FastAPI()
+templates = Jinja2Templates(directory="audio_app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload-audio/")
+async def upload_audio(request: Request, file: UploadFile = File(...)):
+    file_location = f'audio_app/uploads/{file.filename}'
+    with open(file_location, "wb+") as file_object:
+        file_object.write(file.file.read())
+    # Aqui, posso adicionar a lógica para processar áudio e gerar metadados
+    return {"filename": file.filename}
